09434351ac481ec90be2eec13c52ef9b21336f17
LuisHoyos7 <luishoyosolaya@gmail.com>
2021-04-09 15:10:36 -0500

    creation any seeders and models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Group::factory(10)->create();
+        \App\Models\Category::factory(10)->create();
+        \App\Models\UserType::factory(10)->create();
+        \App\Models\User::factory(10)->create();
     }
 }
