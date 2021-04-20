82f8ef8e7b31280ef16b90ceba1c792d33852acc
lorocode90 <73199982+lorocode90@users.noreply.github.com>
2021-04-20 00:28:27 -0500

    change styles cards

--- resources/views/components/commons/card.blade.php
+++ resources/views/components/commons/card.blade.php
@@ -1,9 +1,14 @@
-<div class="card mt-3 shadow-card-1" style="width: 100%;">
+<div class="card mt-3 shadow-card-1">
     <img src="{{ asset('logo/gth.png') }}" class="card-img-top" alt="...">
-    Duracion: {{$course->duration}}
+
+
     <div class="card-body">
-        <h5 class="card-title"></h5>{{$course->name}}
-        <p class="card-text">{{$course->description}}</p>
+        <h5 class="card-title">{{ $course->name }}</h5>
+        <span>
+            Duracion: {{ $course->duration }}
+        </span>
+        <p class="card-text text-justify text-wrap text-truncate" style="height: 6rem; border:1px solid">
+            {{ $course->description }}</p>
         <a href="{{ route('show.course', $course->id) }}" class="btn btn-gth-blue ">comprar</a>
     </div>
 </div>
